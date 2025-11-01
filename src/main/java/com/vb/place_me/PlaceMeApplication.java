cac6cb1d1eb8900119437003097209041e844e49
Victor Barreto <112266976+V1ct0rSb@users.noreply.github.com>
2025-11-01 13:15:24 -0300

    Initial commit

---
+++ src/main/java/com/vb/place_me/PlaceMeApplication.java
@@ -0,0 +1,13 @@
+package com.vb.place_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vb/place_me/PlaceMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vb.place_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
