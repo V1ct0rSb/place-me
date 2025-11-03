54b0d2a91e4e960ef222afc647b21bc60502e523
Victor Barreto <112266976+V1ct0rSb@users.noreply.github.com>
2025-11-03 09:42:18 -0300

    feat: Adicionar os relacionamentos das entities

--- src/main/java/com/vb/place_me/Reserva/entity/ReservaModel.java
+++ src/main/java/com/vb/place_me/Reserva/entity/ReservaModel.java
@@ -3,13 +3,21 @@ package com.vb.place_me.Reserva.entity;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.vb.place_me.Propriedade.entity.PropriedadeModel;
 import com.vb.place_me.Reserva.enums.StatusReserva;
+import com.vb.place_me.Usuario.entity.UsuarioModel;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -29,4 +37,12 @@ public class ReservaModel {
     private LocalDateTime criadoEm;
     @Column(name = "atualizado_em")
     private LocalDateTime atualizadoEm;
+
+    @OneToMany(mappedBy = "reserva")
+    List<PropriedadeModel> propriedades = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    @JsonIgnore
+    private UsuarioModel usuario;
 }
