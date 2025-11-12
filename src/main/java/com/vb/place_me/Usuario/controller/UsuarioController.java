f31c2a0cf1b4c7843dbaa5fc474d6d53c9ac658e
Victor Barreto <112266976+V1ct0rSb@users.noreply.github.com>
2025-11-12 17:02:19 -0300

    feat: adiciona roles de acesso a classe Usuario

--- src/main/java/com/vb/place_me/Usuario/controller/UsuarioController.java
+++ src/main/java/com/vb/place_me/Usuario/controller/UsuarioController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +28,7 @@ public class UsuarioController {
     private final UsuarioService service;
 
     @PostMapping("/usuarios")
+    @PreAuthorize("hasAnyRole('PROPRIETARIO', 'ADMINISTRADOR', 'HOSPEDE')")
     public ResponseEntity<UsuarioResponseDTO> criarUsuario(@Valid @RequestBody UsuarioCreateDTO dto) {
         UsuarioResponseDTO usuarioResponseDTO = service.criarUsuario(dto);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -33,6 +36,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/usuarios")
+    @PreAuthorize("hasAnyRole('PROPRIETARIO', 'ADMINISTRADOR', 'HOSPEDE')")
     public ResponseEntity<List<UsuarioResponseDTO>> listarUsuarios() {
         List<UsuarioResponseDTO> usuarioResponseDTO = service.listarUsuarios();
         return ResponseEntity.ok()
@@ -40,9 +44,18 @@ public class UsuarioController {
     }
 
     @PatchMapping("/usuarios")
+    @PreAuthorize("hasAnyRole('PROPRIETARIO', 'ADMINISTRADOR', 'HOSPEDE')")
     public ResponseEntity<UsuarioResponseDTO> editarUsuario(@RequestBody UsuarioCreateDTO dto, @PathVariable Long id) {
         UsuarioResponseDTO usuarioResponseDTO = service.editarUsuario(dto, id);
         return ResponseEntity.ok()
             .body(usuarioResponseDTO);
+    }
+
+    @DeleteMapping("/usuarios/{id}")
+    @PreAuthorize("hasAnyRole('PROPRIETARIO', 'ADMINISTRADOR', 'HOSPEDE')")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+        service.deletarUsuario(id);
+        return ResponseEntity.noContent()
+            .build();
     }
 }
